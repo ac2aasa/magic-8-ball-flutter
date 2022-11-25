ae27fc74e45db6c45c4ae8fccc3cdbbecb21e0d4
Aryan <ac0453@srmist.edu.in>
2022-11-26 00:18:33 +0530

    third repositary

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Ballpage(),
       ),
     );
+class Ballpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Center(child: Text('ASK ME ANYTHING')),
+      ),
+      body: Ball(),
+    );
+  }
+}
+class Ball extends StatefulWidget {
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballnumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: (){
+          setState(() {
+            ballnumber= Random().nextInt(5)+1;
+          });
+
+        },
+        child: Image.asset('images/ball$ballnumber.png'),
+      ),
+
+    );
+  }
+}
